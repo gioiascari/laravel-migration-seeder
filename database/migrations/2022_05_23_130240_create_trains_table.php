eabbcdadd4c69cdfe497e413cb62790e86adade0
gioiascari <gioia.ascari96@gmail.com>
2022-05-23 19:24:32 +0200

    sistemato

--- database/migrations/2022_05_23_130240_create_trains_table.php
+++ database/migrations/2022_05_23_130240_create_trains_table.php
@@ -19,8 +19,10 @@ class CreateTrainsTable extends Migration
             $table->string('azienda');
             $table->string('stazione_di_partenza');
             $table->string('stazione_di_arrivo');
-            $table->date('orario_di_partenza');
-            $table->date('orario_di_arrivo');
+            $table->time('orario_di_partenza');
+            $table->time('orario_di_arrivo');
+            $table->date('data_di_partenza');
+            $table->date('data_di_arrivo');
             $table->string('codice_treno');
             $table->tinyInteger('numero_carrozze');
             $table->boolean('in_orario');
